e186272cb27d9721bf656d07bf2bd0234c19cadb
Johan Strandberg <johan.alexander.strandberg@hotmail.com>
2016-11-22 16:05:11 +0100

    Added Entity Framework

---
+++ src/GekkoTrading/Models/Entities/Cases.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GekkoTrading.Models.Entities
+{
+    public partial class Cases
+    {
+        public int Id { get; set; }
+        public DateTime CaseDate { get; set; }
+        public int InstrumentId { get; set; }
+        public string UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int BestMaSize1 { get; set; }
+        public int BestMaSize2 { get; set; }
+        public int CandleDuration { get; set; }
+        public double Result { get; set; }
+
+        public virtual Instrument Instrument { get; set; }
+        public virtual AspNetUsers User { get; set; }
+    }
+}
---
+++ src/GekkoTrading/Models/Entities/PriceData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GekkoTrading.Models.Entities
+{
+    public partial class PriceData
+    {
+        public long Id { get; set; }
+        public int InstrumentId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public decimal PriceLow { get; set; }
+        public decimal PriceHigh { get; set; }
+        public decimal PriceAverage { get; set; }
+        public string TimeUnit { get; set; }
+
+        public virtual Instrument Instrument { get; set; }
+    }
+}
