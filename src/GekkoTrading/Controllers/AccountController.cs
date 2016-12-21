e972ad77441fde66b319c9064d137b03c2d882c5
Arvid <arvid.nikka@gmail.com>
2016-12-21 16:50:17 +0100

    Design-update

--- src/GekkoTrading/Controllers/AccountController.cs
+++ src/GekkoTrading/Controllers/AccountController.cs
@@ -137,6 +137,15 @@ namespace GekkoTrading.Controllers
             return RedirectToAction(nameof(HomeController.Index), "Home");
         }
 
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff(string user)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation(4, "User logged out.");
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
         //
         // POST: /Account/ExternalLogin
         [HttpPost]
